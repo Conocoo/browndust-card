6810841f66269c4438ed2e32ec402bbd47f165a0
yk1226ull <yk1226ull>
2024-07-01 02:13:55 +0900

    이미지 경로 변경

--- src/components/BattleBookPage.js
+++ src/components/BattleBookPage.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import './BattleBookPage.css';
-import headImage from '../img/head.png';
 
 function BattleBookPage() {
   return (
     <section id="container" className="battlebookpage">
       <div className="header">
-        <img src={headImage} alt="Header" className="header-image" />
+      <img src={process.env.PUBLIC_URL + '/img/head.png'} alt="Header" className="header-image" />
       </div>
       <div className="content">
         {/* 여기에 콘텐츠를 추가합니다 */}
